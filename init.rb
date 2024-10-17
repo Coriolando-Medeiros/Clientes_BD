90b09bd0f90153634e68cc46c42cbdf8578d6837
Coriolando-Medeiros <coriolando.medeiros@gmail.com>
2024-10-17 05:12:19 -0300

    feat: organizar clientes e fornecedores em 2 funções distintas

--- init.rb
+++ init.rb
@@ -3,40 +3,29 @@ require_relative 'models/fornecedor'
 require 'terminal-table'
 
 
-clientes = Models::Cliente.new
-
-clientes = Models::Cliente.todos
-
-rows = []
-
-rows = clientes.map do |cliente|
-  [cliente.id, cliente.nome, cliente.telefone, cliente.cpf]
+def clientes
+  clientes = Models::Cliente.todos
+  rows = clientes.map do |cliente|
+    [cliente.id, cliente.nome, cliente.telefone, cliente.cpf]
+  end
+  table = Terminal::Table.new(
+      :title => "Lista de Clientes",
+      :headings => ['ID', 'Nome', 'Telefone', 'CPF'],
+      :rows => rows
+  )
+  puts table
 end
 
-table = Terminal::Table.new(
-     :title => "Lista de Clientes",
-     :headings => ['ID', 'Nome', 'Telefone', 'CPF'],
-     :rows => rows
-)
+def fornecedores
+  fornecedores = Models::Fornecedor.todos
+  rows = fornecedores.map do |fornecedor|
+    [fornecedor.id, fornecedor.nome, fornecedor.telefone, fornecedor.cnpj, fornecedor.endereco]
+  end
 
-puts table
-
-puts "-----------------"
-
-fornecedores = Models::Cliente.new
-
-fornecedores = Models::Fornecedor.todos
-
-rows = []
-
-rows = fornecedores.map do |fornecedor|
-  [fornecedor.id, fornecedor.nome, fornecedor.telefone, fornecedor.cnpj, fornecedor.endereco]
+  table = Terminal::Table.new(
+      :title => "Lista de Fornecedores",
+      :headings => ['ID', 'Nome', 'Telefone', 'CNPJ', 'Endereço'],
+      :rows => rows
+  )
+  puts table
 end
-
-table = Terminal::Table.new(
-     :title => "Lista de Fornecedores",
-     :headings => ['ID', 'Nome', 'Telefone', 'CNPJ', 'Endereço'],
-     :rows => rows
-)
-
-puts table
